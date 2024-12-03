5889aa32b2da244de898d7ee36c320b0c28c0e91
Danniel Navas <dannielnavas@gmail.com>
2024-12-03 08:04:32 -0500

    feature(render props): implement Child and Parent components with render prop pattern

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,16 @@
+import React from "react";
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+};
+
+export const ChildComponent: React.FunctionComponent<ChildComponentProps> = ({
+  render,
+}) => {
+  const name = "DannielDev";
+  return <>{render(name)}</>;
+};
+
 export const ParentComponent = () => {
-  return (
-    <>
-    </>
-  );
+  return <ChildComponent render={(name) => <p>Hola {name}</p>} />;
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,15 @@
-const ParentComponent = () => {
+import React, { useState } from "react";
+
+interface ParentComponentProps {
+  render: (data: string[]) => React.ReactNode;
+}
+
+const ParentComponent: React.FunctionComponent<ParentComponentProps> = ({
+  render,
+}) => {
+  const [data] = useState<string[]>(["DannielDev", "React", "TypeScript"]);
+
+  return <ul>{render(data)}</ul>;
 };
 
 export default ParentComponent;
