5872c6bc22aeff5f2adb4c3a470b123fbda86589
Danniel Navas <dannielnavas@gmail.com>
2024-12-05 07:39:15 -0500

    feature(custom hooks): implement useLoading hook and update UserComponent to manage loading state

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,31 @@
-const UserComponent = () => {
+import React, { useState } from "react";
+
+function useLoading(inicialLoading: boolean = false) {
+  const [isLoading, setIsLoading] = useState(inicialLoading);
+  return {
+    isLoading,
+    setIsLoading,
+  };
+}
+
+type UserProps = {
+  name: string;
+};
+
+const UserComponent: React.FC<UserProps> = ({ name }) => {
+  const { isLoading, setIsLoading } = useLoading(false);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
+      <p>Hello, {name}</p>
+      <button onClick={() => setIsLoading(true)}>Load</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name="DannielDev" />;
 };
