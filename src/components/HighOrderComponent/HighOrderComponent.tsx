91445b5906e5d0cc21b85b37ad36f89d604fc521
Danniel Navas <dannielnavas@gmail.com>
2024-12-05 07:31:56 -0500

    feature(high-order component): implement withLoading HOC to manage loading state for components

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,27 @@
+import React from "react";
+
+function withLoading<T extends object>(Component: React.ComponentType<T>) {
+  return (props: T & { isLoading: boolean }) => {
+    const { isLoading, ...rest } = props;
+    return isLoading ? <div>Loading...</div> : <Component {...(rest as T)} />;
+  };
+}
+
+type UserProps = {
+  name: string;
+};
+
+const UserComponent: React.FC<UserProps> = ({ name }) => {
+  return <p>Hola, {name} !</p>;
+};
+
+const UserWithLoading = withLoading(UserComponent);
+
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading name="DannielDev" isLoading={false} />
+      <UserWithLoading name="Danniel Navas" isLoading={true} />
     </div>
   );
 };
