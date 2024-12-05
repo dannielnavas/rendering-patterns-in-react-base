7f96a6efd808d79efd8f908433c3a9417af46092
Danniel Navas <dannielnavas@gmail.com>
2024-12-05 07:22:45 -0500

    feature(compound components): implement Tabs and Tab components with state management patron compound components

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,12 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Tab 1">Tab 1 content</Tab>
+      <Tab label="Tab 2">Tab 2 content</Tab>
+      <Tab label="Tab 3">Tab 3 content</Tab>
+    </Tabs>
   );
 };
