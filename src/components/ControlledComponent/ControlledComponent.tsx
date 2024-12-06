486fc07f6a3edd4265cfbd66a40c5b9e1ef29935
Danniel Navas <dannielnavas@gmail.com>
2024-12-06 07:12:17 -0500

    feature(inputs): implement ControlledInput and UncontrolledInput components with state management

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("");
   return (
     <div>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Ingresa el código del cupón 10% off"
+      />
+      <p>
+        {" "}
+        Cupón de descuento: <b>{value}</b>
+      </p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,21 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`Producto añadido: ${inputRef.current?.value} ⭐`);
+    }
+  };
   return (
     <div>
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="Nombre del producto. Ej Manzana"
+      />
+
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
